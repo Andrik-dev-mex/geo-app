4637d77f4a8dd9e7154e8f9ab91324d6d9154328
Andrik Pineda Castillo <kean5817@gmail.com>
2020-09-25 03:05:30 -0500

    boton de mi ubicacion funcionando

--- src/App.js
+++ src/App.js
@@ -1,34 +1,62 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Appbar from "./layout/Appbar";
 import Busqueda from "./components/Busqueda";
 import Botones from "./components/Botones";
 import Mapa from "./components/Mapa";
 
 function App() {
-  const [lat, setLat] = useState('')
-  const [long, setLong] = useState('')
-  
+  const [lat, setLat] = useState("");
+  const [long, setLong] = useState("");
+
+  let markers = [];
+
+  const setCenter = (lat, long) => {
+    setLat(lat);
+    setLong(long);
+    window.L.mapquest.Map.getMap("map").setView(new window.L.latLng(lat, long), 12);
+    console.log(lat, long)
+  };
+
+  const addMarker = (lat, long, tititle, subtitle) => {
+    const marker = window.L.mapquest.textMarker(
+      new window.L.latLng(lat, long),
+      {
+        text: tititle || 'marcador',
+        subtext: subtitle,
+        position: "right",
+        type: "marker",
+        icon: {
+          primaryColor: "#a8190f",
+          secondaryColor: "#db2c2c",
+          size: "md",
+        },
+      }
+    )
+    .addTo(window.L.mapquest.Map.getMap('map'));
+    markers.push(marker)
+  };
+
   return (
     <div>
       <Appbar />
       <div className="container mt-2">
         <div className="row">
           <div className="col-sm-8">
-            <Busqueda></Busqueda>
+            <Busqueda />
           </div>
           <div className="col-sm-4">
-            <Botones></Botones>
+            <Botones setCenter={setCenter} setMarker={addMarker} />
           </div>
         </div>
         <div className="row">
           <div className="col">
             <Mapa
-              height = "80vh"
-              width = "100%"
-              keyApi = "UiUPhV0Us6gLzXYrrW4BsYbDmo09RapQ"
-              center = {[lat, long]}
-              titleLayer = {"map"}
-              zoom = {12}
+              height="80vh"
+              width="100%"
+              keyApi="UiUPhV0Us6gLzXYrrW4BsYbDmo09RapQ"
+              center={[lat, long]}
+              titleLayer={"map"}
+              zoom={12}
             />
           </div>
         </div>
--- src/components/Botones.jsx
+++ src/components/Botones.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import Swal from "sweetalert2";
 
-export default function Botones(){
+export default function Botones({setCenter, setMarker}){
+  const findMe = () => {
+
+    if(!navigator.geolocation) {
+      Swal.fire(
+        'Error al encontrarte',
+        'Tu navegardor no es compatible',
+        'error'
+      );
+      return;
+    }
+  
+    function succes(position) {
+      let {latitude, longitude} = position.coords;
+      console.log(latitude, longitude);
+      if(setCenter) {
+        setCenter(latitude, longitude);
+      }
+
+      if(setMarker) {
+        setMarker(latitude, longitude);
+      }
+      
+      console.log(latitude, longitude);
+    }
+  
+    function err() {
+      Swal.fire(
+        'Ha ocurrido un error',
+        'Intenta recargar la pagina',
+        'error'
+      );
+    }
+  
+    navigator.geolocation.getCurrentPosition(succes, err);
+  
+  }
+
   return(
     <div className="row">
-      <button className = "btn btn-primary btn-block">
+      <button type = "button" onClick = {findMe}
+        className = "btn btn-primary btn-block" >
         Mi ubicacion
       </button>
     </div>
